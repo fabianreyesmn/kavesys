ec2d2a0829e89938a274b607e8b8e0d2dbd0d740
GusGit2003 <gusavegue@gmail.com>
2025-12-15 22:40:19 -0600

    Corrección de Puertos

--- test/test.js
+++ test/test.js
@@ -1,9 +1,14 @@
 const request = require('supertest');
 const app = require('../index');
+const db = require('../db');
 
 describe('GET /', () => {
   it('Responde con un código de estado 200', async () => {
     const response = await request(app).get('/');
     expect(response.statusCode).toBe(200);
   });
+});
+
+afterAll(async () => {
+  await db.end();
 });
